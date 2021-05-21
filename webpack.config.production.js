ca501c173a18cd84930ee5c539d62f58dfd8e52a
ArminKlacarArtech <82584687+ArminKlacarArtech@users.noreply.github.com>
2021-05-21 10:42:00 +0200

    Update webpack.config.production.js

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -29,6 +29,11 @@ module.exports = {
   plugins: [
     new webpack.optimize.UglifyJsPlugin({
       compress: {warnings: false}
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+          NODE_ENV: JSON.stringify('production'),
+      }
     })
   ]
 };
